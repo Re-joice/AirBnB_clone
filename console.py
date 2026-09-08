2a11531475737000a3a29ec6ddccd35bd5a51bd4
Rejoice Ofosu -Armah <ofosuarmahrejoice@gmail.com>
2026-09-08 15:03:45 -0700

    Add command interpreter console

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Console module."""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the AirBnB project."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program."""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered."""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
