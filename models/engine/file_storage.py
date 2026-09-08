a888513e818bdd37eb28730d8498133132fcb6bf
Rejoice Ofosu -Armah <ofosuarmahrejoice@gmail.com>
2026-09-08 15:55:21 -0700

    Update console and file storage for all classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,12 +39,23 @@ class FileStorage:
 
         from models.base_model import BaseModel
         from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
         classes = {
             "BaseModel": BaseModel,
-            "User": User
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
         }
 
         for key, value in objects_dict.items():
-            class_name = value["__class__"]
-            self.__objects[key] = classes[class_name](**value)
+            class_name = key.split(".")[0]
+            if class_name in classes:
+                self.__objects[key] = classes[class_name](**value)
